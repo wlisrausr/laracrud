cd019d154b804e0981dd4718bd84eae45d1ea2d8
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-06-09 14:23:47 +0700

    Add user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('address');
+            $table->string('phone');
             $table->timestamps();
         });
     }
