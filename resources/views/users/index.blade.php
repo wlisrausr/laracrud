ce92c673c37c8aa4fba4ae2c6e132e876e14185a
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-06-09 18:21:07 +0700

    Add index view for user

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('title')
+  User List
+@endsection
+
+@section('styles')
+  <link href="/css/jquery.dataTables.css" rel="stylesheet">
+  <link href="/css/dataTables.bootstrap.css" rel="stylesheet">
+@endsection
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h2 class="panel-title">User List</h2>
+          </div>
+
+          <div class="panel-body">
+            <p><a class="btn btn-sm btn-primary" href="{{ route('users.create') }}"><i class="fa fa-user-plus"></i> Add</a></p>
+
+            {!! $html->table(['class'=>'table-striped']) !!}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@section('scripts')
+  <script src="/js/jquery.dataTables.min.js"></script>
+  <script src="/js/dataTables.bootstrap.min.js"></script>
+  {!! $html->scripts() !!}
+@endsection
