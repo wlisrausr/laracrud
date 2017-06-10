7faf42f4c5d41633e1982a22f3e705e046846be1
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-06-10 11:28:19 +0700

    Delete pipe in datatable action view

--- resources/views/datatable/_action.blade.php
+++ resources/views/datatable/_action.blade.php
@@ -1,4 +1,4 @@
 {!! Form::model($model, ['url' => $form_url, 'method' => 'delete', 'class' => 'form-inline'] ) !!}
-    <a class="btn btn-xs btn-success" href="{{ $edit_url }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a> |
+    <a class="btn btn-xs btn-success" href="{{ $edit_url }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>
     {!! Form::submit('Delete', ['class'=>'btn btn-xs btn-danger']) !!}
 {!! Form::close()!!}
