a9c26a73457095dedc746c96e903b5e77eb7bd04
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-06-11 13:37:39 +0700

    Add show resource url in datatable action

--- resources/views/datatable/_action.blade.php
+++ resources/views/datatable/_action.blade.php
@@ -1,4 +1,5 @@
 {!! Form::model($model, ['url' => $form_url, 'method' => 'delete', 'class' => 'form-inline'] ) !!}
+    <a class="btn btn-xs btn-primary" href="{{ $show_url }}"><i class="fa fa-user" aria-hidden="true"></i> Show</a>
     <a class="btn btn-xs btn-success" href="{{ $edit_url }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>
     {!! Form::submit('Delete', ['class'=>'btn btn-xs btn-danger']) !!}
 {!! Form::close()!!}
